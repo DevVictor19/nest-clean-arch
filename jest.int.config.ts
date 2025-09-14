efe4cfc5cf0ab40983775097aaae8dc0a7d5ee65
DevVictor19 <antoniovictor12@live.com>
2025-09-14 15:49:05 -0300

    chore: add tests config

---
+++ jest.int.config.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { pathsToModuleNameMapper } from 'ts-jest';
+
+// @ts-ignore
+import { compilerOptions } from './tsconfig.json' with { type: 'json' };
+
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testRegex: '.*\\.int-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
