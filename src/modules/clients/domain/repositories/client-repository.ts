e908386910ce653be46dc3aaba0512d5c5c8f2af
DevVictor19 <antoniovictor12@live.com>
2025-09-20 15:47:56 -0300

    feat: add client repository interface

---
+++ src/modules/clients/domain/repositories/client-repository.ts
@@ -0,0 +1,7 @@
+import { BasePaginatedRepository } from '@/core/domain/repositories/base-paginated-repository';
+import { ClientEntity } from '../entities/client-entity';
+
+export abstract class ClientRepository extends BasePaginatedRepository<ClientEntity> {
+  abstract findByEmail(email: string): Promise<ClientEntity | null>;
+  abstract findByPhone(phone: string): Promise<ClientEntity | null>;
+}
